2162a43d8ac6ca3982a3f47ada95a690486698d4
Janak ALford <info@symaiotics.com>
2022-07-11 06:55:41 -0400

    Updated controller to work in a loop with dist

--- controllers/notifier.js
+++ controllers/notifier.js
@@ -13,27 +13,41 @@ const axios = require('axios').default;
 exports.postControl = async function (req, res, next) {
 
     try {
-
-        var emailUrl = "https://api.notification.canada/v2/notifications/email";
+        var emailUrl = "https://api.notification.canada.ca/v2/notifications/email";
         var emailBody = req.body.emailBody || req.query.emailBody;
-        var emailHeader = req.body.emailBody || req.query.emailBody;
+        var emailHeader = req.body.emailHeader || req.query.emailHeader;
+        var distributionList = req.body.distributionList || req.query.distributionList;
 
-        var startTime = new Date();
-        var results = await axios.post({
-            method: "post",
-            url: emailUrl,
-            headers: emailHeader,
-            data: emailBody
-        })
-        var endTime = new Date();
-        var timeDiff = endTime - startTime;
-        if (timeDiff < 60) {
-            await sleep(60 - timeDiff)
+
+        for (var a = 0; a < distributionList.length; a++) {
+            var thisEmail = JSON.parse(JSON.stringify(emailBody)) //make a fresh copy of the email
+            thisEmail.email_address = distributionList[a].email_address; //Bring the individual email from the distribution list in
+
+            for (const [key, value] of Object.entries(distributionList[a])) {
+                console.log(`${key}: ${value}`);
+                if (key != 'email_address') thisEmail.personalisation[key] = value;
+            }
+            var startTime = new Date();
+            var results = await axios({
+                method: "post",
+                url: emailUrl,
+                headers: emailHeader,
+                data: thisEmail
+            })
+            // var results = true
+            // Add a delay of 60 milliseconds on the total job so the API calls don't exceed 1000 calls in 60 seconds as per GC Notify's requirements
+            var endTime = new Date();
+            var timeDiff = endTime - startTime;
+            console.log(timeDiff)
+            if (timeDiff < 60) {
+                await sleep(60 - timeDiff)
+            }
         }
-        if (results) res.status(200).send({ message: "Message Sent!", payload: results })
+        res.status(200).send({ message: "Message Sent!", payload: true })
 
     }
     catch (error) {
+        console.log(error)
         res.status(500).send({ message: "Error sending email", payload: results })
 
     }
